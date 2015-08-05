904bf445978d28b80342b76fd9ddbee89190578e
OleOle7177 <ole_@bk.ru>
2015-08-05 20:39:15 +0600

    Nokogiti added to Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,10 +75,10 @@ set :scm,             :git
 # домашнем каталоге в подкаталоге git/<имя проекта>.git.
 # Подробнее о создании репозитория читайте в нашем блоге
 # http://locum.ru/blog/hosting/git-on-locum
-set :repository,      "git@github.com:OleOle7177/selection.git"
+# set :repository,      "git@github.com:OleOle7177/selection.git"
 
 ## Если ваш репозиторий в GitHub, используйте такую конфигурацию
-# set :repository,    "git@github.com:username/project.git"
+set :repository,    "https://github.com/OleOle7177/selection.git"
 
 ## --- Ниже этого места ничего менять скорее всего не нужно ---
 
